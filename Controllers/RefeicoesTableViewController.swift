76fb7390d90efe055565f7b1dbf62f6873b60483
user <user@mb033252.local>
2022-06-27 15:58:09 -0300

    Feat: Adicionando a TableViewController.

---
+++ Controllers/RefeicoesTableViewController.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController {
+    
+    let refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
+                     Refeicao(nome: "Pizza", felicidade: 4),
+                     Refeicao(nome: "Comida Japonesa", felicidade: 5)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao.nome
+        
+        return celula
+    }
+    
+}
