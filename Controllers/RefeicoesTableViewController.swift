b8f6d45301e6a9a3b12e8f0c4386add869752ff8
user <user@mb033252.local>
2022-06-28 13:26:18 -0300

    Feat: Implementado o metodo para Adicionar refeicoes.

--- Controllers/RefeicoesTableViewController.swift
+++ Controllers/RefeicoesTableViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class RefeicoesTableViewController: UITableViewController {
     
-    let refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
+    var refeicoes = [Refeicao(nome: "Macarrão", felicidade: 4),
                      Refeicao(nome: "Pizza", felicidade: 4),
                      Refeicao(nome: "Comida Japonesa", felicidade: 5)]
     
@@ -19,4 +19,14 @@ class RefeicoesTableViewController: UITableViewController {
         return celula
     }
     
+    func add(_ refeicao: Refeicao){
+        refeicoes.append(refeicao)
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? ViewController {
+            viewController.tableViewController = self
+        }
+    }
 }
--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var tableViewController: RefeicoesTableViewController?
+    
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet weak var felicidadeTextField: UITextField?
     
@@ -26,6 +28,10 @@ class ViewController: UIViewController {
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
         
         print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        
+        tableViewController?.add(refeicao)
+        
+        navigationController?.popViewController(animated: true)
     }
 }
 
